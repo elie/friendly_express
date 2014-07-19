9c1a347f0e67e2acf74f5a7a894e52f1a856a922
eschoppik <elie@knowyo.com>
2014-07-19 13:30:37 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express'),
+path = require('path'),
+logger = require('morgan'),
+bodyParser = require('body-parser'),
+// cookieParser = require('cookie-parser'),
+// favicon = require('serve-favicon'),
+app = express();
+
+// Middleware
+app.set('port', process.env.PORT || 3000); 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+// app.use(cookieParser());
+// app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', function(req, res) {
+  res.render('index', { title: 'Express', heading: 'Let\'s get started' });
+});
+
+// Get the server started
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+### Express Generator 	
+1. Run npm install
+2. Start with either node app.js or npm start
